93db2890ccb21583f12358ee8abc370a4af52439
ubadineke <ubadinekeprince@gmail.com>
2025-12-30 12:50:44 +0100

    fix for bad gateway issue

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://api.gavern.org/:path*',
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
